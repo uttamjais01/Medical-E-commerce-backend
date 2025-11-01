0c97acadc7300c5a88808fee979edcf6cd13a732
uttam jaiswal <uttamjaiswal09soft@gmail.com>
2025-11-01 15:18:47 +0530

    cors

--- app.js
+++ app.js
@@ -17,9 +17,13 @@ app.use(express.urlencoded({ extended : true}))
 app.use(cookieParser())
 
 const corsOption = {
-    origin : "http://localhost:5173",
-    credentials : true
+    origin: [
+        "http://localhost:5173",
+        "https://medical-e-commerce-frontend-t4xx.vercel.app"
+    ],
+    credentials: true
 }
+
 app.use(cors(corsOption))
 
 app.use('/user',userRoutes)
